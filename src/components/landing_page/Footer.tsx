24e2464fead45090beea8750b1093cd5f6d1e861
Luismkm <luismarcelo13@yahoo.com.br>
2023-03-31 20:54:44 -0300

    refactor: add width max

--- src/components/landing_page/Footer.tsx
+++ src/components/landing_page/Footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return(
-    <footer className='w-screen flex flex-col justify-center items-center h-16 md:h-[60px] bg-[#212121]'>
+    <footer className='w-full max-w-[1120px] mx-auto flex flex-col justify-center items-center h-16 md:h-[60px] bg-[#212121]'>
       <span className='text-sm'>Desenvolvido por <span className='text-[#00DF5E] font-bold'>Luis Moraes</span></span>
       <span className="text-xs">Design por <span className='text-[#00DF5E] font-bold'>Iuri Silva</span></span>
     </footer>
